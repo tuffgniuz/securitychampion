6d5b6efeed95f8803459f00a59fbc50a7d030c20
Imanuel Febie <tuffgniuz.dev>
2024-06-21 14:36:39 +0000

    feat: Enhance subcategory UI and fix filter button borders
    - Add summary field to subcategories
    - Enable collapsing of requirements table by clicking on subcategory title
    - Fix colored border for active filter buttons

--- app/lib/components/requirements-table.tsx
+++ app/lib/components/requirements-table.tsx
@@ -1,11 +1,12 @@
-import { FC } from "react";
-import { LucideCheck } from "lucide-react";
+import { FC, useState } from "react";
+import { LucideCheck, LucideChevronDown, LucideChevronUp } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 import useFilteredRequirements from "../hooks/useFilteredRequirements";
+import usePeek from "../hooks/usePeek";
 
 import BookmarkButton from "./bookmark-button";
 import Peek from "./peek";
-import usePeek from "../hooks/usePeek";
 
 interface Props {
   subCategories: SubCategory[];
@@ -30,6 +31,11 @@ const RequirementsTable: FC<Props> = ({
   );
   const { peekIsVisible, selectedRequirementId, handleRowClick, closePeek } =
     usePeek();
+  const [collapsed, setCollapsed] = useState<{ [key: string]: boolean }>({});
+
+  const toggleCollapse = (id: string) => {
+    setCollapsed((prevState) => ({ ...prevState, [id]: !prevState[id] }));
+  };
 
   const highlightText = (text: string, query: string) => {
     if (!query) return text;
@@ -57,59 +63,84 @@ const RequirementsTable: FC<Props> = ({
         // Exclude tables with no matching requirements
         if (filteredRequirements.length === 0) return null;
 
+        const isCollapsed = collapsed[sc.sub_category_id];
+
         return (
           <div
             key={sc.sub_category_id}
             className="border border-nord-polarnight-100 rounded-lg mb-5 shadow-md"
           >
-            <div className="bg-nord-polarnight-100 rounded-t-lg">
-              <h2 className="flex items-center gap-2 p-2 font-semibold">
-                <span>{sc.sub_category_id}</span>
-                {sc.sub_category_name}
-              </h2>
+            <div className="bg-nord-polarnight-100 p-2 rounded-t-lg">
+              <div
+                onClick={() => toggleCollapse(sc.sub_category_id)}
+                className="mb-2 flex justify-between cursor-pointer"
+              >
+                <h2 className="text-nord-frost-100 flex items-center gap-2 font-semibold">
+                  <span>{sc.sub_category_id}</span>
+                  {sc.sub_category_name}
+                </h2>
+                {isCollapsed ? (
+                  <LucideChevronDown size={20} />
+                ) : (
+                  <LucideChevronUp size={20} />
+                )}
+              </div>
+              <p className="font-thin">{sc?.summary}</p>
             </div>
-            <table className="text-left border-t border-nord-polarnight-100 w-full">
-              <thead className="text-sm">
-                <tr>
-                  <th className="p-2">ID</th>
-                  <th className="p-2">Description</th>
-                  <th className="p-2">L1</th>
-                  <th className="p-2">L2</th>
-                  <th className="p-2">L3</th>
-                  <th className="p-2"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredRequirements.map((r) => (
-                  <tr
-                    key={r.requirement_id}
-                    className="border-t border-nord-polarnight-100 hover:bg-nord-polarnight-25 transition-all duration-300 ease-in-out cursor-pointer"
-                  >
-                    <td className="p-2 text-sm font-thin">
-                      {highlightText(r.requirement_id, searchQuery)}
-                    </td>
-                    <td
-                      className="p-2"
-                      onClick={() => handleRowClick(r.requirement_id)}
-                    >
-                      {highlightText(r.description, searchQuery)}
-                    </td>
-                    <td className="p-2">
-                      {r.level1 && <LucideCheck size={12} />}
-                    </td>
-                    <td className="p-2">
-                      {r.level2 && <LucideCheck size={12} />}
-                    </td>
-                    <td className="p-2">
-                      {r.level3 && <LucideCheck size={12} />}
-                    </td>
-                    <td className="p-2">
-                      <BookmarkButton requirementId={r.requirement_id} />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <AnimatePresence initial={false}>
+              {!isCollapsed && (
+                <motion.div
+                  key="content"
+                  initial={{ height: 0, opacity: 0 }}
+                  animate={{ height: "auto", opacity: 1 }}
+                  exit={{ height: 0, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <table className="text-left border-t border-nord-polarnight-100 w-full">
+                    <thead className="text-sm">
+                      <tr>
+                        <th className="p-2">ID</th>
+                        <th className="p-2">Description</th>
+                        <th className="p-2">L1</th>
+                        <th className="p-2">L2</th>
+                        <th className="p-2">L3</th>
+                        <th className="p-2"></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {filteredRequirements.map((r) => (
+                        <tr
+                          key={r.requirement_id}
+                          className="border-t border-nord-polarnight-100 hover:bg-nord-polarnight-25 transition-all duration-300 ease-in-out cursor-pointer"
+                        >
+                          <td className="p-2 text-sm font-thin">
+                            {highlightText(r.requirement_id, searchQuery)}
+                          </td>
+                          <td
+                            className="p-2"
+                            onClick={() => handleRowClick(r.requirement_id)}
+                          >
+                            {highlightText(r.description, searchQuery)}
+                          </td>
+                          <td className="p-2">
+                            {r.level1 && <LucideCheck size={12} />}
+                          </td>
+                          <td className="p-2">
+                            {r.level2 && <LucideCheck size={12} />}
+                          </td>
+                          <td className="p-2">
+                            {r.level3 && <LucideCheck size={12} />}
+                          </td>
+                          <td className="p-2">
+                            <BookmarkButton requirementId={r.requirement_id} />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
         );
       })}
